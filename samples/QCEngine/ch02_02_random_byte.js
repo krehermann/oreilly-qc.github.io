66989c9274e75ad01b6f6df319127f0988238ef2
krehermann <kreherma@gmail.com>
2021-02-04 08:16:51 -0700

    [chapter-2] Extension of some exerices

--- samples/QCEngine/ch02_02_random_byte.js
+++ samples/QCEngine/ch02_02_random_byte.js
@@ -7,8 +7,8 @@
 // This sample generates a single random byte,
 // using eight unentangled qubits.
 
-qc.reset(8);         // allocate some qubits
+qc.reset(8);         // allocate some qubits. Note that default circle notation view only shows 24 states. There are 2^8=256 states in qubyte
 qc.write(0);         // write the value zero
 qc.had();            // place them all into superposition of 0 and 1
 var result = qc.read();
-qc.print('result: ' + result + '\n');
+qc.print('result: ' + result + ': ' + result.toString(2) + '\n');
